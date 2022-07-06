c812fb9de42d22a3e68d566d150477c3f1d6c32f
jerryl_james <jerryl@elementzonline.com>
2022-07-06 11:39:56 +0530

    EEPROM Emulation

---
+++ EEPROM_Emulation/Core/Inc/eeprom_emul.h
@@ -0,0 +1,37 @@
+
+/**
+  
+  This is a free software under the GNU license, you can redistribute it and/or modify it under the terms
+  of the GNU General Public License version 3 as published by the Free Software Foundation.
+  This software library is shared with public for educational purposes, without WARRANTY and Author is not liable for any damages caused directly
+  or indirectly by this software, read more about this on the GNU General Public License.
+  ***************************************************************************************************************
+*/
+
+#ifndef EEPROM_EMUL_H_
+#define EEPROM_EMUL_H_
+
+#include "stm32f1xx_hal.h"		//Change here acc to series
+
+
+uint32_t Flash_Write_Data (uint32_t StartPageAddress, uint32_t *Data, uint16_t numberofwords);
+
+void Flash_Read_Data (uint32_t StartPageAddress, uint32_t *RxBuf, uint16_t numberofwords);
+
+void Convert_To_Str (uint32_t *Data, char *Buf);
+
+void Flash_Write_NUM (uint32_t StartSectorAddress, float Num);
+
+float Flash_Read_NUM (uint32_t StartSectorAddress);
+
+
+
+/********************  FLASH_Error_Codes   ***********************//*
+HAL_FLASH_ERROR_NONE      0x00U  // No error
+HAL_FLASH_ERROR_PROG      0x01U  // Programming error
+HAL_FLASH_ERROR_WRP       0x02U  // Write protection error
+HAL_FLASH_ERROR_OPTV      0x04U  // Option validity error
+*/
+
+
+#endif 
